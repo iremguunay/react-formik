a971c601e05e8be7baafe050d96bb3d19218fbb6
Irem Gunay <iremguunay@gmail.com>
2022-06-23 20:24:19 +0300

    Form validation and error handling

--- src/components/validation.js
+++ src/components/validation.js
@@ -2,9 +2,9 @@ import { ref } from "yup";
 import { object, string, number, date, InferType } from "yup";
 
 const validations = object({
-  email: string().email().required(),
-  password: string().min(6).required(),
-  passwordConfirm: string().oneOf([ref('password')]).required(),
+  email: string().email('Geçerli bir email girin').required('Zorunlu alan'),
+  password: string().min(6, 'Parolanın en az 6 karakter olmalı').required('Zorunlu alan'),
+  passwordConfirm: string().oneOf([ref('password')], 'Parolanız uyuşmuyor').required('Zorunlu alan'),
 });
 
 export default validations;
